68741840d98f1b48e6a4966892be2be530958ff8
Kunwoo Yoon <ykw0628@gmail.com>
2022-11-08 19:20:48 -0500

    connected nodeExpress to mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,39 @@
 //app.js will hold the express app
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose
+  .connect(
+    'mongodb+srv://milesAdmin:7VN6saOdG72gNTlA@cluster0.d0ejdlc.mongodb.net/node-miles?retryWrites=true&w=majority'
+  )
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
-//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//middleware to prevent CORS error
 app.use((req, res, next) => {
-    console.log('middleware');
-    next();
+  res.setHeader('Access-Control-Allow-origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
 });
 
 app.use((req, res, next) => {
-    res.send('Hello from express');
+  res.send('Hello from express');
 });
 
 module.exports = app;
