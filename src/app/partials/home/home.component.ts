656e8b56d49bb4ab8e6e95d9ab802a37ef1b4d1e
Sina <snpazhwak@gmail.com>
2022-11-19 18:20:53 -0500

    home and sign up

---
+++ src/app/partials/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.sass']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/partials/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
