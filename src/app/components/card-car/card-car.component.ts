8000dda2ff00da40b05f2818e2b983580923e5e0
Andy NaZag <andynava57@gmail.com>
2022-11-20 16:09:10 -0500

    Search bar component completed

---
+++ src/app/components/card-car/card-car.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-car',
+  templateUrl: './card-car.component.html',
+  styleUrls: ['./card-car.component.scss']
+})
+export class CardCarComponent implements OnInit {
+  @Input() data!: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
