4eade12be45eac68f515b6776c0f3ddff6e3e648
Kunwoo Yoon <ykw0628@gmail.com>
2022-11-21 08:28:31 -0500

    Revert "Merge branch 'main' into login-register-integration"
    This reverts commit c2b08f3120f015559779c9bb67a221da37c65c57.

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,11 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.sass']
+  styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
+  @Input() placeholder: String = 'Search';
+  @Output() newItemEvent = new EventEmitter<string>();
+
+  addNewLocation(value: string) {
+    this.newItemEvent.emit(value);
+  }
 
   constructor() { }
 
