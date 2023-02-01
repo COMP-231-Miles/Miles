dc893de7b02f7650aa1d3ee2f7cd3e5f9f67f51f
Kunwoo Yoon <ykw0628@gmail.com>
2023-02-01 15:21:20 -0500

    feat: added filter ui on the car list

--- src/app/components/filter/filter.component.ts
+++ src/app/components/filter/filter.component.ts
@@ -5,11 +5,67 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './filter.component.html',
   styleUrls: ['./filter.component.scss']
 })
+
 export class FilterComponent implements OnInit {
+  types = [
+    {
+      id: 1,
+      title: 'cars',
+      checked: false,
+    },
+    {
+      id: 2,
+      title: 'suvs',
+      checked: false,
+    },
+    {
+      id: 3,
+      title: 'trucks',
+      checked: false,
+    },
+    {
+      id: 4,
+      title: 'vans',
+      checked: false,
+    },
+  ]
+
+  numberOfPassengers = [
+    {
+      id: 1,
+      title: '2+',
+      checked: false,
+    },
+    {
+      id: 2,
+      title: '4+',
+      checked: false,
+    },
+    {
+      id: 3,
+      title: '5+',
+      checked: false,
+    },
+    {
+      id: 4,
+      title: '6+',
+      checked: false,
+    },
+    {
+      id: 5,
+      title: '7+',
+      checked: false,
+    },
+    {
+      id: 6,
+      title: '8+',
+      checked: false,
+    },
+  ]
 
   constructor() { }
 
   ngOnInit() {
-  }
 
+  }
 }
